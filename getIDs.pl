9d2f9faa2bdcd2d1a8e3b839ce489c69ed4f1cfb
alexMAF93 <31741850+alexMAF93@users.noreply.github.com>
2018-07-22 18:18:49 +0300

    Update getIDs.pl

--- getIDs.pl
+++ getIDs.pl
@@ -6,7 +6,7 @@ use DBI;
 
 
 my $ORACLE_HOME = "/opt/oracle/product/11203_ee_64/db";
-my $ORACLE_SID="OQUATPRD";
+my $ORACLE_SID="DBNAME";
 $ENV{ORACLE_HOME}=$ORACLE_HOME;
 $ENV{ORACLE_SID}=$ORACLE_SID;
 $ENV{PATH}="$ORACLE_HOME/bin";
@@ -14,7 +14,7 @@ $ENV{PATH}="$ORACLE_HOME/bin";
 my $i = @ARGV[0];
 
 
-my $dbh = DBI->connect('dbi:Oracle:host=vopo01cl-scan.dc-ratingen.de;service_name=OQUATPRD_TAF.prod.vis;port=33000','oquatadmin', 'Qa_Sch_0', { RaiseError => 1, AutoCommit => 0 }) or die "Couldn't open database: $DBI::errstr \n; stopped";
+my $dbh = DBI->connect('dbi:Oracle:host=someserver-scan.dc-ratingen.de;service_name=DB.prod.vis;port=330','user', 'password', { RaiseError => 1, AutoCommit => 0 }) or die "Couldn't open database: $DBI::errstr \n; stopped";
 
         my $sth = $dbh->prepare("select TESTITEM.ID, NAME from TESTITEM 
 where CRQ_ID = '$i' and TIER2_ID in ('100', '78', '39', '24', '29', '30', '6', '7', '8', '9') ORDER BY NAME ASC")
