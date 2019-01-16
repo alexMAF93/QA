82aa8f7e003d4fb29bd200053d1d75689c231dd0
alexMAF93 <31741850+alexMAF93@users.noreply.github.com>
2019-01-16 14:48:23 +0200

    Add files via upload

--- tampering_files.py
+++ tampering_files.py
@@ -9,18 +9,20 @@ def remove_output_file(file):		# used to delete the output file before rewriting
 		pass
 		
 		
-def get_ASIC(dir):
+def get_ASIC(dir, TYPE = "ASIC"):
 	list_of_asics = []              # the ASICs from a CRQ folder
 	list_of_timestamps = []         # the date each ASIC was added
+	ASIC = []						# the list with ASICs in reverse chronological order
 	for i in os.listdir(dir):
-		p = re.match(".*ASIC.*", i, re.IGNORECASE)
+		p = re.match(".*" + TYPE + ".*", i, re.IGNORECASE)
 		if p:
 			if i[0] != "~":         # to avoid trying to get data from an open file
 				list_of_asics.append(i)
-				list_of_timestamps.append(os.path.getctime(dir+"\\"+i))
-	if len(list_of_asics) == 0:		# if the list is empty, no ASIC was found
-		print("NOT_OK:No ASIC found")
-	else:
+				list_of_timestamps.append(os.path.getctime(dir+"/"+i))
+	while len(list_of_timestamps) > 0:
 		the_most_recent_asic = max(list_of_timestamps)
-		ASIC = list_of_asics[list_of_timestamps.index(the_most_recent_asic)]
-		return ASIC					# only the latest ASIC is needed
+		index_the_most_recent_asic = list_of_timestamps.index(the_most_recent_asic)
+		ASIC.append(list_of_asics[index_the_most_recent_asic])
+		list_of_asics.remove(list_of_asics[index_the_most_recent_asic])
+		list_of_timestamps.remove(list_of_timestamps[index_the_most_recent_asic])
+	return ASIC					# ASICs in reverse chronological order
