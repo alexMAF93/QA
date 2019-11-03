c04fa099262f7a77d46745fe6c45ceb752b8d639
alexMAF93 <31741850+alexMAF93@users.noreply.github.com>
2019-11-03 19:44:08 +0200

    Update getIDs.pl

--- getIDs.pl
+++ getIDs.pl
@@ -14,7 +14,7 @@ $ENV{PATH}="$ORACLE_HOME/bin";
 my $i = @ARGV[0];
 
 
-my $dbh = DBI->connect('dbi:Oracle:host=vopo01cl-scan.dc-ratingen.de;service_name=OQUATPRD_TAF.prod.vis;port=33000','oquatadmin', 'Qa_Sch_0', { RaiseError => 1, AutoCommit => 0 }) or die "Couldn't open database: $DBI::errstr \n; stopped";
+my $dbh = DBI->connect('dbi:Oracle:host=vopo01cl-scan.dc-ratingen.de;service_name=OQUATPRD_TAF.prod.vis;port=33000','oquatadmin', '***', { RaiseError => 1, AutoCommit => 0 }) or die "Couldn't open database: $DBI::errstr \n; stopped";
 
         my $sth = $dbh->prepare("select TESTITEM.ID, NAME from TESTITEM 
 where CRQ_ID = '$i' and TIER2_ID in ('3', '40', '39', '6', '7', '8', '9', '22', '23', '24', '78', '100', '26', '27', '28', '29', '30', '31', '88', '89') ORDER BY NAME ASC")
